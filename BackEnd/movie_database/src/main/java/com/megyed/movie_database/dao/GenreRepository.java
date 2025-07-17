4d6c1f4c3ef25fc80e09be2911c0a392ee3e9d29
Egyed Márton <marci20020327@gmail.com>
2025-07-17 18:06:58 +0200

    feat: implement API requests with genres names instead there references

--- BackEnd/movie_database/src/main/java/com/megyed/movie_database/dao/GenreRepository.java
+++ BackEnd/movie_database/src/main/java/com/megyed/movie_database/dao/GenreRepository.java
@@ -4,7 +4,11 @@ import com.megyed.movie_database.entity.Genre;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.List;
+import java.util.Set;
+
 @RepositoryRestResource(path = "genres")
 public interface GenreRepository extends JpaRepository<Genre, Integer> {
+    Set<Genre> findByNameIn(List<String> names);
 
 }
