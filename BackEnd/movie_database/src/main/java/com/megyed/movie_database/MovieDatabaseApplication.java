0f3ce9cf6efd262dff01c4b73e85b59a97825e5c
Egyed Márton <marci20020327@gmail.com>
2025-07-18 18:25:54 +0200

    fixed

--- BackEnd/movie_database/src/main/java/com/megyed/movie_database/MovieDatabaseApplication.java
+++ BackEnd/movie_database/src/main/java/com/megyed/movie_database/MovieDatabaseApplication.java
@@ -2,12 +2,25 @@ package com.megyed.movie_database;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class MovieDatabaseApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieDatabaseApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("*");
+			}
+		};
 	}
 
 }
